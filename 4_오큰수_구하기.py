170b56e7f142cd8d7cc78f33d1af23572b5a2fbf
hanjoo7 <jeffrey7108@gmail.com>
2025-03-11 23:36:51 +0900

    update

--- 4_오큰수_구하기.py
+++ 4_오큰수_구하기.py
@@ -1,3 +1,4 @@
+## 복습 필요
 n = int(input())
 ans = [0] * n
 A = list(map(int, input().split()))
@@ -7,3 +8,12 @@ for i in range(n):
     while myStack and A[myStack[-1]] < A[i]:
         ans[myStack.pop()] = A[i]
     myStack.append(i)
+
+while myStack:
+    ans[myStack.pop()] = -1
+
+result = ""
+for i in range(n):
+    result += str(ans[i]) + " "
+
+print(result)
